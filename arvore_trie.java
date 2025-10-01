cc6facf7fd7b5a73a021b0fc85f558de5eb312ac
lucasferraz122 <lucasfersan10@gmail.com>
2025-09-30 22:51:56 -0300

    criação da estrutura inicial
    criei a classe arvore_trie a classe interna nó e o construtor

--- arvore_trie.java
+++ arvore_trie.java
@@ -1,1 +1,19 @@
+public class arvore_trie {
+    private static final int tam_alfabeto = 26;
+    private static final int tam_max_palvra = 200;
 
+    private static class no {
+        no[] filhos;
+        boolean fimdapalavra;
+        no() {
+            filhos = new no[tam_alfabeto];
+            fimdapalavra = false;
+        }
+    }
+
+    private no raiz;
+
+    public arvore_trie() {
+        raiz = new no();
+    }
+}
